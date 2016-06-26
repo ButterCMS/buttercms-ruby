16063a8d781933984cad30df19c5b92de6f11738
Chase Coleman <chase@buttercms.com>
2016-06-25 20:52:47 -0500

    returns object instead of hash for content fields

--- lib/buttercms/content.rb
+++ lib/buttercms/content.rb
@@ -1,7 +1,21 @@
 module ButterCMS
   class Content
+    attr_reader :data
+
+    def initialize(json)
+      @json = json
+      @data = HashToObject.convert(json["data"])
+    end
+
+    def inspect
+      id_string = (self.respond_to?(:id) && !self.id.nil?) ? " id=#{self.id}" : ""
+      "#<#{self.class}:0x#{self.object_id.to_s(16)}#{id_string}> JSON: " + JSON.pretty_generate(@json)
+    end
+
     def self.fetch(keys = [])
-      ButterCMS.request("/content", {keys: keys.join(',')})
+      response = ButterCMS.request("/content", {keys: keys.join(',')})
+
+      self.new(response)
     end
   end
 end
